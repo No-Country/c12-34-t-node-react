d92022d3464e912862ad1181dd7d1479531aa34e
Cemb93 <karloz13m@gmail.com>
2023-07-05 00:09:32 -0500

    CRUD de Elementos

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,37 +1,30 @@
-import express, { Application } from "express"
-import "dotenv/config"
-import cors from "cors"
-import { db } from "./db"
-import morgan from "morgan"
-import "../src/models/User"
-import userRoutes from "./routes/user"
+import express, { Application } from "express";
+import "dotenv/config";
+import cors from "cors";
+import { db } from "./db";
+import morgan from "morgan";
+import "../src/models/User";
+import allRoutes from "./routes/routes";
 
-
-
-
-const app: Application = express()
-
+const app: Application = express();
 
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(morgan("dev"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
+app.use(allRoutes);
 
-app.use(userRoutes)
-
-
-const port = process.env.PORT || 3002
-
+const port = process.env.PORT || 3002;
 
 db.sync({ force: false }).then(async () => {
   try {
     app.listen(port, () => {
-      console.log(`Escuchando en el puerto: http://localhost:${port}`)
-    })
+      console.log(`Escuchando en el puerto: http://localhost:${port}`);
+    });
     await db.authenticate();
-    console.log('Conexion a sequelize');
+    console.log("Conexion a sequelize");
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 });
