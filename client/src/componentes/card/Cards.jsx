1ecac4ad08e454750c0eb5ff0b1a8498a7351453
NicoHub98 <78396936+NicoHub98@users.noreply.github.com>
2023-07-05 01:49:25 -0300

    react-icons / Card

---
+++ client/src/componentes/card/Cards.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ItemCard from "./ItemCard";
+
+const Card = () => {
+  // **********
+  // Por el momento, esto funciona como un placeholder
+  // **********
+  const data = [
+    {
+      id: 0,
+      name: "Jhon",
+      position: "Designer",
+      logo: "https://picsum.photos/200",
+    },
+    {
+      id: 1,
+      name: "Carl",
+      position: "Frontend",
+      logo: "https://picsum.photos/200",
+    },
+    {
+      id: 2,
+      name: "Doe",
+      position: "Backend",
+      logo: "https://picsum.photos/200",
+    },
+  ];
+  return (
+    <>
+      <div className="flex flex-wrap gap-4 justify-evenly">
+        {data.map(({ id }) => (
+          <div key={id}>
+            <ItemCard data={data[id]} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Card;
