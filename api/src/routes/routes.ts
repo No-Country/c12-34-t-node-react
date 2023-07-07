8429438f52c09b435e37717317e10e6a7707ada2
jerlibgnzlz <jerlibgv@gmail.com>
2023-07-07 16:26:31 -0300

    middleware de proteger rutas

--- api/src/routes/routes.ts
+++ api/src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 import {
   registerUser,
-  loginUser
+  loginUser,
+  perfil
 } from "../controllers/user"
 import { deleteElementsGym, getElementsGym, postElementsGym, putElemetsGym } from "../services/getElements"
+import { authToken } from "../middlewares/authToken"
 
 const allRoutes = Router()
 
@@ -11,7 +13,9 @@ allRoutes.post("/api/auth", registerUser)
 
 allRoutes.post("/api/login", loginUser)
 
-allRoutes.get("/api/elements", getElementsGym);
+
+allRoutes.get("/api/elements", authToken, getElementsGym);
+
 allRoutes.post("/api/elements", postElementsGym);
 allRoutes.put("/api/elements/:id", putElemetsGym);
 allRoutes.delete("/api/elements/:id", deleteElementsGym);
