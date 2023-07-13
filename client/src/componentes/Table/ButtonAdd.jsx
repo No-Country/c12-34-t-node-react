e1c0d257972114c290343d23331c0c1461ac441c
NicoHub98 <78396936+NicoHub98@users.noreply.github.com>
2023-07-13 18:41:10 -0300

    secciones clientes/proveedores/staff, titulo, tabla

---
+++ client/src/componentes/Table/ButtonAdd.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+
+const ButtonAdd = ({ tBody, setTBody, tHeader, type }) => {
+  const [inputField, setInputField] = useState([]);
+  const addElement = () => {
+    const notNull = inputField.filter((x) => x);
+
+    if (notNull.length < tHeader.length) {
+      return toast.error("Debes llenar todos los campos");
+    }
+    let newId;
+    if (tBody.length < 1) {
+      newId = 1;
+    } else {
+      newId = tBody[tBody.length - 1].id + 1;
+    }
+    let newElement;
+    /* -------------------------------------------------------------------------- */
+    /*                              Definiendo tipos                              */
+    /* -------------------------------------------------------------------------- */
+    if (type === "clientes") {
+      newElement = [
+        {
+          id: newId,
+          nombre: inputField[0],
+          plan: inputField[1],
+          celular: inputField[2],
+          mail: inputField[3],
+          inicio: inputField[4],
+          vencimiento: inputField[5],
+        },
+      ];
+    } else if (type === "proveedores") {
+      newElement = [
+        {
+          id: newId,
+          name: inputField[0],
+          product: inputField[1],
+          contact: inputField[2],
+          mail: inputField[3],
+          date: inputField[4],
+          description: inputField[5],
+          provider: inputField[6],
+        },
+      ];
+    }
+
+    setTBody((items) => [...items, ...newElement]);
+    // console.log(tBody);
+    setInputField([]);
+    toast.success("Nuevo elemento agregado");
+  };
+  function handleChange(i, e) {
+    const values = [...inputField];
+    values[i] = e.target.value;
+    setInputField(values);
+  }
+  return (
+    <div className="flex justify-center">
+      {/* The button to open modal */}
+      <label
+        htmlFor="my_modal_add"
+        className="btn text-white px-10 bg-[#1A1363] hover:bg-black"
+      >
+        Agregar
+      </label>
+
+      <input type="checkbox" id="my_modal_add" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg text-center">Agregar</h3>
+          <div className="grid gap-2">
+            {tBody &&
+              tHeader.map((item, i) => {
+                // if (i < 6) {
+                return (
+                  <div key={i}>
+                    <label className="block" htmlFor={`input-${i}`}>
+                      {item}
+                    </label>
+                    <input
+                      id={`input-${i}`}
+                      value={inputField[i] || ""}
+                      onChange={(e) => handleChange(i, e)}
+                      type="text"
+                      className="input input-bordered w-full"
+                    />
+                  </div>
+                );
+                // }
+              })}
+          </div>
+          <div className="modal-action">
+            <label
+              onClick={addElement}
+              className="btn text-white px-10 bg-[#1A1363] hover:bg-black"
+              htmlFor="my_modal_add"
+            >
+              Guardar
+            </label>
+            <label
+              htmlFor="my_modal_add"
+              className="btn border-[#1A1363] btn-outline px-10 hover:bg-black"
+            >
+              Cancelar
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonAdd;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,44 +1,49 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import { TableV1, Bienes,BienesMaquinas } from '../../componentes/index'
-import Sidebar from './Components/Sidebar'
-import Horarios from './Components/Horarios'
-import Footer from './Components/Footer'
+import {
+  Bienes,
+  BienesMaquinas,
+  SectionClients,
+  SectionProvider,
+  SectionStaff,
+} from "../../componentes/index";
+import Sidebar from "./Components/Sidebar";
+import Horarios from "./Components/Horarios";
+import Footer from "./Components/Footer";
 
 const HomePrincipal = () => {
-  
   return (
-
     <>
-      <div className='flex'>
-
-
+      <div className="flex">
         <div>
           <Sidebar />
         </div>
 
-
         <Routes>
-
-          <Route path='/admin' element={<h1>acá iria el componente admin</h1>} />
-          <Route path='/bienes' element={<Bienes />} />
-          <Route path='/bienes/maquinas' element={<BienesMaquinas/>} />
-          <Route path='/staff' element={<h1 className='m-auto'>acá iria el componente staff</h1>} />
-          <Route path='/clientes' element={<TableV1 />} />
-          <Route path='/proovedores' element={<h1>acá iria el componente proovedores</h1>} />
-          <Route path='/horarios' element={<Horarios />} />
-          <Route path='/reservas' element={<h1>acá iria el componente reservas</h1>} />
-          <Route path='/gastos' element={<h1>acá iria el componente gastos</h1>} />
-
+          <Route
+            path="/admin"
+            element={<h1>acá iria el componente admin</h1>}
+          />
+          <Route path="/bienes" element={<Bienes />} />
+          <Route path="/bienes/maquinas" element={<BienesMaquinas />} />
+          <Route path="/staff" element={<SectionStaff />} />
+          <Route path="/clientes" element={<SectionClients />} />
+          <Route path="/proovedores" element={<SectionProvider />} />
+          <Route path="/horarios" element={<Horarios />} />
+          <Route
+            path="/reservas"
+            element={<h1>acá iria el componente reservas</h1>}
+          />
+          <Route
+            path="/gastos"
+            element={<h1>acá iria el componente gastos</h1>}
+          />
         </Routes>
-
       </div>
 
       <Footer />
-
     </>
+  );
+};
 
-  )
-}
-
-export default HomePrincipal
+export default HomePrincipal;
