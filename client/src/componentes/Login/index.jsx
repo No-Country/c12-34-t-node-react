5f0fd03954714794e26f251dcf33c45f66f03307
Octavio Salas <salasoctavio129@gmail.com>
2023-07-13 23:36:52 -0300

    finDeContexto

--- client/src/componentes/Login/index.jsx
+++ client/src/componentes/Login/index.jsx
@@ -46,7 +46,10 @@ const Login = () => {
 
     
         swAlert(<h2> Bienvenido {userName} </h2>);
-       navigate('/admin');
+        setTimeout(() => { 
+          navigate(`/admin/${userCtx.userId}`);
+        }, 500)
+       
         if (name.length === 0) {
           setNotName(true);
         }
