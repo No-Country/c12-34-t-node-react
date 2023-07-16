a8511e8e9b6dd9a2f9d1eb15cf78d6c4076bdbe8
Cemb93 <karloz13m@gmail.com>
2023-07-16 03:15:29 -0500

    return to data base

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,22 +1,22 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-// export const db = new Sequelize(
-//   `${process.env.DB_NAME}`,
-//   `${process.env.DB_USER}`,
-//   `${process.env.DB_PASSWORD}`,
-//   {
-//     host: `${process.env.DB_HOST}`,
-//     dialect: "postgres",
-//     logging: false,
-//   }
-// );
-
 export const db = new Sequelize(
-  `${process.env.DEPLOY}`,
+  `${process.env.DB_NAME}`,
+  `${process.env.DB_USER}`,
+  `${process.env.DB_PASSWORD}`,
   {
     host: `${process.env.DB_HOST}`,
     dialect: "postgres",
     logging: false,
   }
 );
+
+// export const db = new Sequelize(
+//   `${process.env.DEPLOY}`,
+//   {
+//     host: `${process.env.DB_HOST}`,
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
