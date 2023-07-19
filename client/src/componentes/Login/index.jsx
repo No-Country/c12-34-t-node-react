96466d16d79fc441c4ccbb7f855f2e1c6182db54
Franco <francosaldano1@gmail.com>
2023-07-18 23:27:31 -0300

    Login responsive

--- client/src/componentes/Login/index.jsx
+++ client/src/componentes/Login/index.jsx
@@ -1,29 +1,30 @@
-import axios from "axios";
-import { useState } from 'react';
-import { Link, Navigate, useNavigate } from "react-router-dom";
-import Section from "../Section";
-import { CgAsterisk } from 'react-icons/cg';
-import 'react-toastify/dist/ReactToastify.css';
-import swAlert from "@sweetalert/with-react";
-import { useContext } from "react";
-import { UserContext } from "../../store/userContext";
+import axios from "axios"
+import { useState } from 'react'
+import { Link, Navigate, useNavigate } from "react-router-dom"
+import Section from "../Section"
+import { CgAsterisk } from 'react-icons/cg'
+import 'react-toastify/dist/ReactToastify.css'
+import swAlert from "@sweetalert/with-react"
+import { useContext } from "react"
+import { UserContext } from "../../store/userContext"
+import logo from "./assets/Group 409ss.png"
 
 const Login = () => {
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [notName, setNotName] = useState(false);
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const [notName, setNotName] = useState(false)
 
   const userCtx = useContext(UserContext)
 
   const logUser = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     const userData = {
       email: name,
       password: password
-    };
+    }
 
     axios
       .post(`${import.meta.env.VITE_BACKEND_URL}/api/login`, userData)
@@ -32,129 +33,107 @@ const Login = () => {
         console.log(res.data.data.user.user)
         console.log(res.data.data.user.email)
         console.log(res.data.data.token)
-        
-         userCtx.updateUser(res.data.data.user.id)
-         userCtx.updateUserNameRegistered(res.data.data.user.user)
-         userCtx.updateUserEmailRegistered(res.data.data.user.email)
-         userCtx.updateUserTokenRegistered(res.data.data.token)
+
+        userCtx.updateUser(res.data.data.user.id)
+        userCtx.updateUserNameRegistered(res.data.data.user.user)
+        userCtx.updateUserEmailRegistered(res.data.data.user.email)
+        userCtx.updateUserTokenRegistered(res.data.data.token)
 
 
-         //estas 3 líneas son necesarias para loguear hasta que actualicemos todo lo que utiliza el token del sessionStorage
-         const user = res.data;
-         const newToken = user.data.token
-         sessionStorage.setItem('userToken', newToken);
-        
-        swAlert(<h2> Bienvenido {user.data.user.user} </h2>);
-        setTimeout(() => { 
-          navigate('/home/admin');
+        //estas 3 líneas son necesarias para loguear hasta que actualicemos todo lo que utiliza el token del sessionStorage
+        const user = res.data
+        const newToken = user.data.token
+        sessionStorage.setItem('userToken', newToken)
+
+        swAlert(<h2> Bienvenido {user.data.user.user} </h2>)
+        setTimeout(() => {
+          navigate('/home/admin')
         }, 500)
-       
+
         if (name.length === 0) {
-          setNotName(true);
+          setNotName(true)
         }
       })
 
       .catch(err => {
-        console.log(err);
-        swAlert(<h2>{err.response.data.msg}</h2>);
-        if (err.response.data.msg === 'Esta cuenta no está registrada')
-        {
+        console.log(err)
+        swAlert(<h2>{err.response.data.msg}</h2>)
+        if (err.response.data.msg === 'Esta cuenta no está registrada') {
           navigate('/register')
-        } 
+        }
 
-        
+
         console.log(err.response.data.msg)
-      });
-  };
- 
-    let token = sessionStorage.getItem('userToken')
-    // let token = userCtx.userTokenRegistered
-  
+      })
+  }
+
+  let token = sessionStorage.getItem('userToken')
+  // let token = userCtx.userTokenRegistered
+
 
   return (
     <>
-    {token !== null 
-    ? <Navigate to='/home' />
-    :
-      <Section>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '10px', height:'calc(100vh - 70px)'}}>
-          <Link to="/">
-            <svg style={{ marginLeft: '65px' }} width="120" height="120" viewBox="0 0 484 476" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M303 248.174C303 352.46 235.171 437 151.5 437C67.8288 437 0 352.46 0 248.174C0 143.888 67.8294 343.394 151.501 343.394C235.172 343.394 303 143.888 303 248.174Z" fill="#F9D51C" />
-              <path d="M53 230.4C53 342.466 217.821 230.4 108.745 230.4C168.473 27.4862 272 436.243 272 324.177C272 212.11 -0.329771 0 108.745 0C217.821 0 53 118.334 53 230.4Z" fill="#343A40" />
-            </svg>
+      {token !== null
+        ? <Navigate to='/home' />
+        :
+        <main
+          className='flex items-center justify-center gap-10sm:gap-0 my-4 mx-2 sm:mx-5 md:mx-10'>
+          <Link to="/" className='basis-1/2 mx-auto hidden md:block'>
+            <img src={logo} alt="Logo de Fitness Center Gym" className='basis-1/2 w-[30vw] mx-auto ' />
           </Link>
-          <div className="w-300">
-            <div style={{ boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.3)' }} className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 rounded-lg bg-gray-200 p-4 w-[50vh]">
-              <div className="sm:mx-auto sm:w-full sm:max-w-sm " >
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-black">Inicio de sesión</h2>
-              </div>
-
-          <form action="#" method="POST">
-              <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6" action="#" method="POST">
-                  <div>
-
-                    <div className="flex items-center align-middle">
-                      <label htmlFor="email" className="block text-sm font-medium leading-6 text-black"> Email </label>
-                      <CgAsterisk color='black' />
-                    </div>
-           
- 
-
-                    <div className="mt-2">
-                      <input style={{ padding: '5px' }} id="email" name="email" placeholder='Email' type="email" autoComplete="email" required onChange={(e) => setName(e.target.value)}
-                        className="block w-full rounded-md  py-1.5  bg-black  text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                    </div>
+          <div className="basis-1/2 w-[50vh] flex flex-1 flex-col gap-5 justify-center p-6 sm:py-12 lg:px-8 rounded-lg bg-gray-200 bg-opacity-60 shadow-md ">
+            <h2 className="text-center text-2xl font-PoppinsBold leading-9 tracking-tight text-pallete-black">Inicio de sesión</h2>
+            <form className='w-full' action="#" method="POST" >
+              <form className="w-full space-y-6" action="#" method="POST">
+                <div>
+                  <div className="flex items-center align-middle">
+                    <label htmlFor="email" className="block text-sm font-PoppinsSemibold leading-6 text-pallete-black after:content-['*']"> Correo </label>
                   </div>
 
-                  {!!notName && <p>Debes introducir el nombre</p>}
-
-                  <div>
-                    <div className="flex items-center">
-                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-black">Contraseña</label>
-                      <CgAsterisk color='black' />
-
-                    </div>
-                    <div className="mt-2">
-                      <input style={{ padding: '5px' }} id="password" name="password" placeholder='Contraseña' type="password" autoComplete="current-password" required onChange={(e) => setPassword(e.target.value)}
-                        className="block w-full rounded-md border-rounded py-1.5  bg-black  text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-white focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
+                  <div className="mt-2">
+                    <input id="email" name="email" placeholder='Correo' type="email" autoComplete="email" required onChange={(e) => setName(e.target.value)}
+                      className="input input-sm block w-full rounded-md font-PoppinsRegular bg-pallete-black text-pallete-white ring-1 ring-inset ring-pallete-grey focus:ring-2 focus:ring-inset focus:ring-pallete-blue sm:text-sm sm:leading-6" />
                   </div>
+                </div>
 
+                {!!notName && <p>Debes introducir el nombre</p>}
 
-                  <label className="label cursor-pointer">
-                    <span className="label-text">Recuérdame</span>
-                    <input style={{ border: '1px solid' }} type="checkbox" className="checkbox" />
-                  </label>
-
-
-                  <div>
-                    <button
-                      onClick={logUser}
-                      className="flex w-full justify-center rounded-md bg-amber-400 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm0 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Aceptar
-                    </button>
+                <div>
+                  <div className="flex items-center align-middle">
+                    <label htmlFor="password" className="block text-sm font-PoppinsSemibold leading-6 text-pallete-black after:content-['*']">Contraseña</label>
                   </div>
-                </form>
+                  <div className="mt-2">
+                    <input id="password" name="password" placeholder='Contraseña' type="password" autoComplete="current-password" required onChange={(e) => setPassword(e.target.value)}
+                      className="input input-sm block w-full rounded-md font-PoppinsRegular bg-pallete-black text-pallete-white ring-1 ring-inset ring-pallete-grey focus:ring-2 focus:ring-inset focus:ring-pallete-blue sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
 
-                <p className="mt-10 text-center text-sm text-gray-500">
-                  No estás registrado?
-                  <Link to="/register" className="font-semibold leading-6 text-lime-600" style={{ padding: '10px' }} >Crear cuenta</Link>
-                </p>
-              </div>
+                <label className="label cursor-pointer">
+                  <input type="checkbox" className="checkbox checkbox-sm border " />
+                  <span className="label-text ml-2 mr-auto font-PoppinsMedium ">Recuérdame</span>
+                </label>
+
+                <button
+                  onClick={logUser}
+                  className="btn btn-md px-10 py-1.5 mx-auto rounded-xl flex justify-center text-sm shadow-sm font-PoppinsSemibold bg-pallete-yellow hover:bg-pallete-yellow  leading-6 text-pallete-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pallete-blue"
+                >
+                  Aceptar
+                </button>
+              </form>
+
+              <p className="mt-10 text-center text-xs sm:text-sm font-PoppinsSemibold text-pallete-grey">
+                ¿No estás registrado?
+                <Link to="/register" className="p-3 font-PoppinsSemibold leading-6 text-pallete-green">Crear cuenta</Link>
+              </p>
+
             </form>
-            </div>
           </div>
-        </div>
-
-      </Section>
-    }
+        </main>
+      }
     </>
 
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
