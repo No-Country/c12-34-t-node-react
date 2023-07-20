6babcd504abfe0c62534c0ba63335fa9a82e20a7
jerlibgnzlz <jerlibgv@gmail.com>
2023-07-20 16:14:21 -0300

    demo3

--- api/src/services/crudProvider/get.ts
+++ api/src/services/crudProvider/get.ts
@@ -1,12 +1,7 @@
 import { Request, Response } from "express"
 import { Elements, Provider, User, } from "../../models/relations"
 
-<<<<<<< HEAD
-export const getProvider = async (_req: Request, res: Response) => {
-  console.log("Me llego algo")
-=======
 export const getProvider = async (req: Request, res: Response) => {
->>>>>>> 81b58aa5e09cbd3d2f6217df143490902d7be629
   try {
     const proveedores = await Provider.findAll({
       include: {
