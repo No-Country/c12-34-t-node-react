cb53a98e3506e7340cf95b03c317fabcc2b499d5
Octavio Salas <salasoctavio129@gmail.com>
2023-07-21 16:27:55 -0300

    lskndksnd

--- api/src/models/Clientes.ts
+++ api/src/models/Clientes.ts
@@ -25,13 +25,16 @@ Client.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    plan: {
+      type: DataTypes.STRING,
+    },
+    contact: {
+      type: DataTypes.STRING,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-    },
-    plan: {
-      type: DataTypes.STRING,
     },
     dateIn: {
       type: DataTypes.STRING,
@@ -39,9 +42,7 @@ Client.init(
     dateOut: {
       type: DataTypes.STRING,
     },
-    contact: {
-      type: DataTypes.STRING,
-    },
+   
   },
   {
     sequelize: db,
