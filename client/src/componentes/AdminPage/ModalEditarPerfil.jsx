0efb10c435bf1ea2dad5cff107c718e1e1f1181d
JDR89 <joaquin.romero89@gmail.com>
2023-07-25 12:39:46 -0300

    fix adminpage

--- client/src/componentes/AdminPage/ModalEditarPerfil.jsx
+++ client/src/componentes/AdminPage/ModalEditarPerfil.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 import profesor from "../../img/profesor.jpg"
 import axios from 'axios'
 import 'react-toastify/dist/ReactToastify.css';
@@ -36,55 +36,77 @@ const ModalEditarPerfil = () => {
 
   return (
     <div>
-       <button className="btn btn-active btn-warning mt-[4rem] px-10" onClick={()=>window.my_modal_3.showModal()}>Editar Perfil</button>
-                <dialog id="my_modal_3" className="modal">
-                   <form method="dialog" className="modal-box bg-slate-400">
-                   <div >
-                        <div>
-                            <h3 className="font-bold text-lg text-center"><b>Editar Perfil</b> </h3>
-                         </div>
+      <button
+        className="btn btn-active btn-warning mt-4 px-10"
+        onClick={() => window.my_modal_3.showModal()}
+      >
+        Editar Perfil
+      </button>
 
-                         <div className='bg-white rounded-xl'>
-                             <div className='flex mt-[15px]'>
-                                <div className='mt-[10px] ml-[20px]'>
-                                    <p className='text-[15px]'><b>Nombre Completo*</b></p>
-                                    <input type='text' className='bg-slate-300 rounded-lg' onChange={(e) => setName(e.target.value)}></input>
-                                       <p className='text-[15px] mt-[20px]' ><b>Email*</b></p>
-                                       <input type='text' className='bg-slate-300 rounded-lg'  onChange={(e) => setEmail(e.target.value)}></input>
-                                </div>
+      <dialog id="my_modal_3" className="modal">
+        <form method="dialog" className="modal-box bg-gradient-to-b from-[#B0BAC3] to-gray-100">
+          <div className="flex flex-col">
+            <div  >
+              <h3 className=" text-center font-[Poppins] font-bold">
+                Editar Perfil
+              </h3>
+            </div>
 
-                                <div className='ml-[50px] mt-[10px]'>
-                                <div className='mt-[10px] ml-[20px]'>
-                                    <p className='text-[15px]' ><b>Cargo*</b></p>
-                                    <input type='text' className='bg-slate-300 rounded-lg' onChange={(e) => setCargo(e.target.value)}></input>
-                                       <p className='text-[15px] mt-[20px]'><b>Imagen*</b></p>
-                                       <div className="avatar mt-[5px]">
-                                              <div className="w-24 rounded-full">
-                                                 <img src={profesor} />
-                                              </div>
-                                       </div>
-                                </div>
-                                </div>
-                                
-                            </div>
+            <div className="bg-white rounded-xl mt-2 ">
+              <div className=" mt-4 md:mt-0 flex flex-col md:flex-row md:items-center  mx-[1rem]">
 
-                            <div className='flex m-[50px] ml-[110px]'>
-                                
-                                <div>
-                                <button className='text-white btn btn-active btn-primary rounded-xxl w-[70px]' onClick={() => editProfile()}>Guardar</button>
-                                </div>
+                <div className="flex flex-col md:mt-0 md:ml-8">
+                  <p className="text-[15px]">
+                    <b>Nombre Completo*</b>
+                  </p>
+                  <input
+                    type="text"
+                    className="bg-slate-300 rounded-lg"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  ></input>
+                  <p className="text-[15px] mt-5">
+                    <b>Email*</b>
+                  </p>
+                  <input
+                    type="text"
+                    className="bg-slate-300 rounded-lg"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  ></input>
+                </div>
 
-                                <div className='ml-[90px] mb-[15px]'>
-                                    <button className='text-white btn btn-active btn-primary rounded-xxl w-[70px]'>Cancelar</button>
-                                </div>
-                                
-                            </div>
-                                </div>
-                   </div>
-                   </form>
-                </dialog>
+                <div className="md:ml-5 md:mt-10 mb-4 md:mb-10">
+                  <p className="text-[15px] mt-5">
+                    <b>Imagen*</b>
+                  </p>
+                  <div className="avatar mt-2">
+                    <div className="w-24 rounded-full">
+                      <img src={profesor} alt="Imagen de perfil" />
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div className="flex mt-8 justify-center">
+              <button
+                className="hover:bg-[#FFFFFF] hover:text-[#1A1363] font-[Poppins] font-semibold px-14 py-4 text-white btn btn-active bg-[#1A1363] rounded-xxl w-[70px] mr-4 "
+                onClick={() => editProfile()}
+              >
+                Guardar
+              </button>
+              <button className="hover:bg-[#FFFFFF] hover:text-[#1A1363] font-[Poppins] font-semibold px-14 py-4 text-white btn btn-active bg-[#1A1363] rounded-xxl w-[70px] mr-4 ">
+                Cancelar
+              </button>
+            </div>
+          </div>
+        </form>
+      </dialog>
     </div>
-  )
+  );
 }
 
 export default ModalEditarPerfil
