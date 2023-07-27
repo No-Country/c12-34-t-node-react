98b3139e78d02a26b81b36f7023ca7cd0ab22714
Franco <francosaldano1@gmail.com>
2023-07-26 22:45:38 -0300

    demasiado pequeño el titulo en mobile

--- client/src/componentes/Title/index.jsx
+++ client/src/componentes/Title/index.jsx
@@ -7,7 +7,7 @@ const Title = ({ title }) => {
       <header className='hidden sm:flex justify-center items-center '>
         <img className='w-32 ' src={Logo} alt="FitnessCenterLogoGym logo de la marca y aplicación web Fitness Center" />
       </header>
-      <h3 className='self-start text-base sm:text-xl md:text-2xl mx-auto sm:m-0 px-4 sm:p-0 font-PoppinsBold border-b-2 border-spacing-14 text-pallete-black border-pallete-green inline'>
+      <h3 className='self-start text-xl sm:text-xl md:text-2xl mx-auto sm:m-0 px-4 sm:p-0 font-PoppinsBold border-b-2 border-spacing-14 text-pallete-black border-pallete-green inline'>
         {title}
       </h3>
     </>
